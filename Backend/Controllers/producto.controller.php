a1365f76099442082cca726d128445961da123c7
Akio1102 <cristiandaviddiazaza@gmail.com>
2024-03-15 09:36:58 -0500

    fix: (Controllers) :pencil2: Arreglo tipografico

--- Backend/Controllers/producto.controller.php
+++ Backend/Controllers/producto.controller.php
@@ -18,7 +18,7 @@ switch ($op) {
         if (isset($body["id"])) {
             $datos = $producto->getProductoId($body["id"]);
         } else {
-            $datos = ["error" => "Se requiere un ID para obtener una categoría"];
+            $datos = ["error" => "Se requiere un ID para obtener una Producto"];
         }
         break;
 
@@ -26,7 +26,7 @@ switch ($op) {
         if (isset($body["code"]) && isset($body["name"]) && isset($body["category_id"]) && isset($body["price"])) {
             $datos = $producto->postProducto($body["code"], $body["name"], $body["category_id"], $body["price"]);
         } else {
-            $datos = ["error" => "Se requiere un nombre para agregar una categoría"];
+            $datos = ["error" => "Se requiere un nombre para agregar una Producto"];
         }
         break;
 
@@ -34,7 +34,7 @@ switch ($op) {
         if (isset($body["id"]) && isset($body["code"]) && isset($body["name"]) && isset($body["category_id"]) && isset($body["price"])) {
             $datos = $producto->putProducto($body["id"], $body["code"], $body["name"], $body["category_id"], $body["price"]);
         } else {
-            $datos = ["error" => "Se requiere un ID y un nombre para modificar una categoría"];
+            $datos = ["error" => "Se requiere un ID y un nombre para modificar una Producto"];
         }
         break;
 
@@ -42,7 +42,7 @@ switch ($op) {
         if (isset($body["id"])) {
             $datos = $producto->deleteProducto($body["id"]);
         } else {
-            $datos = ["error" => "Se requiere un ID para eliminar una categoría"];
+            $datos = ["error" => "Se requiere un ID para eliminar un Producto"];
         }
         break;
 
